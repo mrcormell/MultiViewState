5ce98c3b5307ff7e9fc8e256962a91d2ca732c21
David Cormell <d.cormell@etoncollege.org.uk>
2023-02-23 13:50:23 +0000

    Initial Commit

---
+++ MultiViewState/MultiViewStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiViewStateApp.swift
+//  MultiViewState
+//
+//  Created by Cormell, David - DPC on 23/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MultiViewStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
